3626d75313fe155be6db57566a4b075294b36502
PRITI-MJ <104292906+PRITI-MJ@users.noreply.github.com>
2024-03-14 18:22:36 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -56,3 +56,37 @@ console.log(typeof stringNumber);// string
 
 
 
+// ***************** Operations ************
+
+let value = 3
+let negValue = -value
+console.log(negValue) //3
+
+//console.log(2**3) //2^3(2 to the power of 3)
+
+let str1 = "hello"
+let str2 = "priti"
+
+let str3 = str1 + str2
+console.log(str3); //hellopriti
+
+console.log("1" + 2) //12
+console.log(1 + "2") //12
+console.log("1" + 2 + 2) //122
+console.log(1 + 2 + "2" + 7 + 4) //3274 
+//once the string comes the numbers after that will be treated as string
+
+console.log(true)//true
+console.log(+true) //1
+// console.log(true+) //error
+console.log(+"")//0
+console.log(+false)//0
+
+let num1, num2, num3 
+num1 = num2 = num3 = 2 + 2
+
+console.log(num2); //4
+
+let gameCounter = 100;
+gameCounter++; //if(++gameCounter) => same ans
+console.log(gameCounter);//101
