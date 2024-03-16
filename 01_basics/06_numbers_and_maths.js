1df5b9fca347bddeb300669e1d2943eb34ca4697
PRITI-MJ <104292906+PRITI-MJ@users.noreply.github.com>
2024-03-16 17:17:53 +0000

    math and num

--- 01_basics/06_numbers_and_maths.js
+++ 01_basics/06_numbers_and_maths.js
@@ -23,4 +23,29 @@ console.log(otherNumber.toPrecision(4)); //123.9
 console.log(otherNumber.toPrecision(2));//1.2e+2
 
 
+const hundreds = 1000000
+console.log(hundreds.toLocaleString())//1,000,000 //it gives the value in US standards
+console.log(hundreds.toLocaleString('en-IN')) //10,00,000 //it gives the value in INDIAN standards
+
+
+// ++++++++++++++++++++ Maths +++++++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-4)); //4
+console.log(Math.round(4.6)); //5
+console.log(Math.ceil(4.2)); //5
+console.log(Math.floor(4.9)); //4
+console.log(Math.min(4, 3, 6, 8)); //3
+console.log(Math.max(4, 3, 6, 8)); //8
+
+console.log(Math.random()); //it always gives values from 0(inclusive) to 1(exclusive) //0.7486472392961403
+console.log((Math.random()*10) + 1); //to get the minimum value of 1 //10.680401219991637
+console.log(Math.floor(Math.random()*10) + 1); //to get the integer values from 1 to 10
+
+const min = 10
+const max = 20
+
+//to get the values between 10 to 20(both inclusive)
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+//here (max-min+1) gives the range and (+min) decides the starting point
 
