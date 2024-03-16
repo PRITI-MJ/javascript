312ca8b330a69524d9bcc1c2f10bee1a025df7ab
PRITI-MJ <104292906+PRITI-MJ@users.noreply.github.com>
2024-03-16 20:37:50 +0000

    array part 2

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -94,4 +94,75 @@ array after applying splice=  [ 0, 4, 5, 6 ]
 //splice make changes in the original array => it removes the spliced elements from the original array
 
 
+const marvel_heros = ["thor", "Ironman", "spiderman"]
+const dc_heros = ["superman", "flash", "batman"]
 
+marvel_heros.push(dc_heros);
+
+console.log(marvel_heros);
+
+//[ 'thor', 'Ironman', 'spiderman', [ 'superman', 'flash', 'batman' ] ]
+//it takes the dc_heros in the array, and treat as a element in the array
+console.log(marvel_heros[3]); //[ 'superman', 'flash', 'batman' ]
+
+marvel_heros.pop();
+
+const allHeros = marvel_heros.concat(dc_heros);
+console.log(allHeros);// [ 'thor', 'Ironman', 'spiderman', 'superman', 'flash', 'batman' ] 
+//it treats every element differently
+
+const heros = ["shaktiman", "naagraj"]
+
+
+//we can also use spread operator(mostly used)
+const all_new_heros = [...marvel_heros, ...dc_heros, ...heros]; //with the help of spread operator we can add as many arrays we can
+console.log(all_new_heros);
+
+/*
+[
+  'thor',      'Ironman',
+  'spiderman', 'superman',
+  'flash',     'batman',
+  'shaktiman', 'naagraj'
+]
+*/
+
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+
+//Returns a new array with all sub-array elements concatenated into it recursively up to the specified depth.
+const real_another_array = another_array.flat(Infinity);
+console.log(real_another_array);
+/*
+[
+  1, 2, 3, 4, 5,
+  6, 7, 6, 7, 4,
+  5
+]
+*/
+
+const real_another_array1 = another_array.flat(1); //depth = 1(it takes only upto one level of subarray)
+console.log(real_another_array1); 
+//[ 1, 2, 3, 4, 5, 6, 7, 6, 7, [ 4, 5 ] ]
+
+
+const real_another_array2 = another_array.flat(2); //depth = 2(it takes only upto 2nd level of subarray)
+console.log(real_another_array2);
+/*
+[
+  1, 2, 3, 4, 5,
+  6, 7, 6, 7, 4,
+  5
+]
+*/
+
+
+console.log(Array.isArray("priti")) //false
+console.log(Array.from("priti")); //[ 'p', 'r', 'i', 't', 'i' ]
+console.log(Array.from({name: "priti"})) //[] => it returns an empty array because in case of object we need to also define the keys
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3)) //it will convert this numbers into arrays
+//[ 100, 200, 300 ]
